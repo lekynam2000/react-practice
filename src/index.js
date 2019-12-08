fe1d20dcc5e47b1055b1d43d78d1a8fc60c08cdc
lekynam2000 <lekynam2000@gmail.com>
2019-12-08 22:24:24 +0700

    change2

--- src/index.js
+++ src/index.js
@@ -1,58 +1,109 @@
 import React from "react"
-import {render} from "react-dom"
+import { render } from "react-dom"
 import "./style.scss"
+
 const likebtn = document.querySelector(".like");
-class Status extends React.Component{
-    constructor(props){
+function getData(city){
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=";
+    var openweatherApiKey = '7b55b40b533897f929d79c9d5869e072';
+    // var googleApiKey = "AIzaSyBkub2p7uuExPJYt0UaeUkpcZFpNzadqKI";
+    // var ggurl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+city+"&inputtype=textquery&fields=photos&key="+googleApiKey;
+    fetch(url+city+"&appid="+openweatherApiKey)
+    .then((response)=>{
+        if(!response.ok){
+            console.log("problem");
+        }
+        console.log(response.json())})
+    .catch((e)=>console.log(e));
+    // fetch(ggurl)
+    // .then((response)=>{
+    //     if(!response.ok){
+    //         console.log("Error");
+    //     }
+    //     console.log(response.json());
+    // })
+};
+class Status extends React.Component {
+    constructor(props) {
         super(props),
-        this.state={
-            likes: 0,
-            comment: 0,
-            commentList:["ok"],
-        }
-        
+            this.state = {
+                likes: 0,
+                comment: 0,
+                commentList: [],
+            }
+        this.submit = this.submit.bind(this);
         this.upLike = this.upLike.bind(this);
-        this.input= React.createRef();
+        this.reset = this.reset.bind(this);
+        this.input = React.createRef();
     }
-    
-    upLike(){
-        this.setState((state)=>{
-            likes: state.likes+1;
-        })
+
+    upLike() {
+        getData("London");
+        this.setState((state) => ({
+            likes: state.likes + 1,
+        }))
     }
-    
-    
-    submit(event){
-        if(event.keyCode==13){
-            this.setState((state)=>({
-                commentList: [...state.commentList,this.input.current.value]
+    reset(event) {
+        if (event.keyCode == 13) {
+            this.input.current.value = "";
+        }
+
+    }
+
+    submit(event) {
+        if (event.keyCode == 13) {
+            this.setState((state) => ({
+                comment: state.comment + 1,
+                commentList: [...state.commentList, this.input.current.value]
             }))
+            console.log("sf" + this.state.comment);
+
         }
     }
+
     
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <div class='status'>
-                    <span>{this.state.likes} likes, {this.state.comment} comments</span>
-                </div>
-                <div class="comment">
-                    {this.state.commentList.map(function(element){
-                        console.log("do");
-                        return <div class="text">{element}</div>;
-                    })}    
-                    <input ref={this.input} type='text' class="comment" 
-                    onKeyDown = {this.submit}
-                    ></input>
-                </div>
+                <section class="container">
+                    <div class="info">
+                        <div class="img"></div>
+                        <div class="name">
+                        <span>Nam Le</span><br/>
+                        <span id="time">now</span>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div class="main"></div>
+                        <div class="like" onClick={this.upLike}>
+                        <span><i class="far fa-thumbs-up"></i>&nbsp; Like</span>
+                        </div>
+                        <div class="comment">
+                        <span><i class="far fa-comment"></i>&nbsp; Comment</span>
+                        </div>
+                    </div>
+                    <div class='status'>
+                        <span>{this.state.likes} likes, {this.state.comment} comments</span>
+                    </div>
+                    <div class="comment">
+                        {this.state.commentList.map(function (element) {
+
+                            return <div class="text">{element}</div>;
+                        })}
+                        <input ref={this.input} type='text' class="comment"
+                            onKeyDown={this.submit}
+                            onKeyUp={this.reset}
+                        ></input>
+                    </div>
+                </section>
+                
             </div>
-            
+
 
         )
     }
 }
 render(
-    <Status ref = {(status)=>{window.status = status}}/>,
+    <Status ref={(component) => { window.component = component }} />,
     document.querySelector("#root")
 );
-document.querySelector(".like").addEventListener("click",status.upLike);
